ee4494c52ff4cb81d2865e1d893a1c29630e2e2f
Gurchet Rai <gurch101@gmail.com>
2020-01-26 23:16:36 -0800

    Updates

---
+++ precache-manifest.b554f231dfac4c8525451c53ba65b693.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897e853576fe7eb9c6c4909c0115efd5",
+    "url": "/pianojs/index.html"
+  },
+  {
+    "revision": "b2c27db89bc32772f4a7",
+    "url": "/pianojs/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "0971d1f675511a39ed12",
+    "url": "/pianojs/static/css/main.d4d5d188.chunk.css"
+  },
+  {
+    "revision": "b2c27db89bc32772f4a7",
+    "url": "/pianojs/static/js/2.d6e903a7.chunk.js"
+  },
+  {
+    "revision": "54b783692fecc6145b89ba7f44eb5a31",
+    "url": "/pianojs/static/js/2.d6e903a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0971d1f675511a39ed12",
+    "url": "/pianojs/static/js/main.b88cdc9f.chunk.js"
+  },
+  {
+    "revision": "81f59d0ac92ae815f52e",
+    "url": "/pianojs/static/js/runtime-main.479edd95.js"
+  },
+  {
+    "revision": "a2f5513e5faa497b3fa68834270b67fe",
+    "url": "/pianojs/static/media/lacrimosa.a2f5513e.mid"
+  },
+  {
+    "revision": "9302bc1a7af2195d87509441d0be2485",
+    "url": "/pianojs/static/media/rachmaninoff.9302bc1a.mid"
+  },
+  {
+    "revision": "323042321b9de4a20f87ad244033a23e",
+    "url": "/pianojs/static/media/wheresmymind.32304232.mid"
+  }
+]);
